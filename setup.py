daff76e74f0a1ac8ef79d70d6256ba38ca557e80
imsubha27 <imsubha27@gmail.com>
2024-12-21 09:32:28 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 # setup.py
-
 from setuptools import setup, find_packages
 
 setup(
     name="pyspark-hello-world",
     version="0.0.1",
-    description="A simple Pyspark application"
-    packages=find_packages(where="src"),   #Automatically detects the python packages under src/.
+    author="Subhankar Das",
+    author_email="imsubha27@gmail.com",
+    description="A simple Pyspark application",
+    packages=find_packages(),   #Automatically detects the python packages under src/.
     package_dir={"": "src"},
     install_requires=[     #Lists dependencies for the package
-        'pyspark>=3.0.0',  # Specify the minimum version of PySpark
+        'pyspark==3.5.4',  # Specify the minimum version of PySpark
     ],
     entry_points={         #Create a CLI command for the app
         'console_scripts': [
             'hello-world = hello-world:main',
         ],
     },
+    python_requires=">=3.6",
 )
